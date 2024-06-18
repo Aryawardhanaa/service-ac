21b78870ae41eaddcfab7ee1b8da6304575993bf
baploarya <aryawardhana7831@gmail.com>
2024-06-18 14:33:00 +0700

    perbaikan

---
+++ src/components/BrandArea.jsx
@@ -0,0 +1,397 @@
+
+const BrandArea = () => {
+    return (
+        < > <div className="brand-area grey-bg pt-100 pb-100">
+            <div className="container">
+                <div className="brand-active swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events">
+                    <div
+                        className="swiper-wrapper"
+                        id="swiper-wrapper-10adabb37f9972510e"
+                        aria-live="off"
+                        style={{
+                            transform: "translate3d(-2400px, 0px, 0px)",
+                            transitionDuration: "0ms"
+                        }}
+                    >
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay=".6s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={1}
+                            role="group"
+                            aria-label="1 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.6s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-2.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay=".9s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={2}
+                            role="group"
+                            aria-label="2 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.9s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-3.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay="1.2s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={3}
+                            role="group"
+                            aria-label="3 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.2s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-4.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate swiper-slide-duplicate-prev"
+                            data-wow-delay="1.5s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={4}
+                            role="group"
+                            aria-label="4 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.5s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-5.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate swiper-slide-duplicate-active"
+                            data-wow-delay="1.8s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={5}
+                            role="group"
+                            aria-label="5 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.8s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-1.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate-next"
+                            data-wow-delay=".3s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={0}
+                            role="group"
+                            aria-label="6 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.3s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-1.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp"
+                            data-wow-delay=".6s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={1}
+                            role="group"
+                            aria-label="7 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.6s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-2.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp"
+                            data-wow-delay=".9s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={2}
+                            role="group"
+                            aria-label="8 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.9s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-3.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp"
+                            data-wow-delay="1.2s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={3}
+                            role="group"
+                            aria-label="9 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.2s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-4.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-prev"
+                            data-wow-delay="1.5s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={4}
+                            role="group"
+                            aria-label="10 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.5s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-5.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-active"
+                            data-wow-delay="1.8s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={5}
+                            role="group"
+                            aria-label="11 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.8s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-1.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate swiper-slide-next"
+                            data-wow-delay=".3s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={0}
+                            role="group"
+                            aria-label="12 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.3s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-1.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay=".6s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={1}
+                            role="group"
+                            aria-label="13 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.6s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-2.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay=".9s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={2}
+                            role="group"
+                            aria-label="14 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "0.9s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-3.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate"
+                            data-wow-delay="1.2s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={3}
+                            role="group"
+                            aria-label="15 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.2s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-4.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                        <div
+                            className="brand-wrapper swiper-slide wow fadeInUp swiper-slide-duplicate swiper-slide-duplicate-prev"
+                            data-wow-delay="1.5s"
+                            data-swiper-autoplay={5000}
+                            data-swiper-slide-index={4}
+                            role="group"
+                            aria-label="16 / 16"
+                            style={{
+                                width: 210,
+                                visibility: "visible",
+                                animationDelay: "1.5s",
+                                animationName: "fadeInUp",
+                                marginRight: 30
+                            }}
+                        >
+                            <a href="#">
+                                <img
+                                    src="/assets/img/brand/brand-5.png"
+                                    className="img-fluid"
+                                    alt="img"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <span
+                        className="swiper-notification"
+                        aria-live="assertive"
+                        aria-atomic="true"
+                    />
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default BrandArea
